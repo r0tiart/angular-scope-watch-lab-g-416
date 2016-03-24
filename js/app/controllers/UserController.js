b30c831b7b7094f5e572afe6c211547bc6891928
Todd Motto <todd@toddmotto.com>
2016-03-24 03:00:20 +0000

    chore: Added solution

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,31 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this;
 
-	this.country = {
+	ctrl.username = '';
+
+	ctrl.country = {
 		name: {
 
 		}
 	};
+
+	$scope.$watch(function () {
+		return ctrl.username;
+	}, function (newValue, oldValue) {
+		console.log('Username updated', newValue);
+	});
+
+	$scope.$watch(function () {
+		return ctrl.country;
+	}, function (newValue) {
+		console.log('Country info updated');
+	}, true);
+
+	$scope.$watchCollection(function () {
+		return ctrl.country.name;
+	}, function (newValue) {
+		console.log('Country name updated');
+	});
 }
 
 angular
