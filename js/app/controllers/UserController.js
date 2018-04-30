3e84b94eb7ed6ba7e0d6724a3af0ba2ec639f84b
r0tiart <antley.chu@gmail.com>
2018-04-30 12:31:19 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -6,6 +6,24 @@ function UserController() {
 
 		}
 	};
+
+	$scope.$watch(function () {
+		return ctrl.username;
+	}, function (newValue, oldValue) {
+		console.log('Username updated', newValue);
+	});
+
+	$scope.$watch(function () {
+		return ctrl.country;
+	}, function (newValue) {
+		console.log('Country info updated');
+	}, true);
+
+	$scope.$watchCollection(function () {
+		return ctrl.country.name;
+	}, function (newValue) {
+		console.log('Country name updated');
+	});
 }
 
 angular
